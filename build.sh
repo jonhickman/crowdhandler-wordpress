56da8c692f4d4ab9b9f88a150dc030e008df331d
Mateusz Chyła <mateusz.chyla@made.media>
2021-02-18 12:43:01 +0100

    Remove some files from default build script

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ rm -rf crowdhandler-wordpress-*.zip && \
 composer install --no-dev --no-interaction && \
 composer dump-autoload --optimize --classmap-authoritative --no-dev --no-interaction
 cd .. && \
-zip -q -0 -r crowdhandler-wordpress-${tag}.zip ${dir} && \
+zip -q -0 -r crowdhandler-wordpress-${tag}.zip ${dir} -x "*.git*" -x "*.idea*" -x "*build.sh*" -x "*.editorconfig*" && \
 cd ${dir} && \
 mv ../crowdhandler-wordpress-${tag}.zip . && \
 rm -rf vendor/composer && \
